e6c650ac27bcd979ccf0bef5cf13df24794f95c5
Mario Posada <marioposadamiranda@gmail.com>
2022-09-12 23:07:25 -0300

    [feature] - implement SafeAreaView &  Touchable

--- App.js
+++ App.js
@@ -1,41 +1,69 @@
-import { FlatList, Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useEffect, useState } from "react";
 
-import Cardetail from "./src/components/CardDetail";
+import CardDetail from "./src/components/CardDetail";
+import axios from "axios";
+
+// import {useFetch} from './src/hooks/useFetch'
 
 export default function App() {
+  const friends = [
+    { id: 1, name: "Anakin Skywalker", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 2, name: "Luke Skywalker", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 3, name: "Obi Wan", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 4, name: "Leia Organa", url: '../../assets/Anakyn_Skywalker.jpeg' },
+    { id: 5, name: "Han Solo", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 6, name: "Yoda", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 7, name: "R2-D2", url: '../../assets/Anakyn_Skywalker.jpeg'},
+    { id: 8, name: "Chewbacca", url: '../../assets/Anakyn_Skywalker.jpeg'},
+  ]
 
-
-  
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../starwars-app/assets/fondoapp.jpeg")}
-        style={styles.image}
-      >
-        <View style={styles.wrap}>
-          {/* <Text style={styles.text}>Mario</Text> */}
-          <Image
-            style={styles.logo}
-            source={require("../starwars-app/assets/logostarwars.png")}
+   <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../starwars-app/assets/fondoapp.jpeg")}
+          style={styles.image}
+        >
+          <View style={styles.wrap}>
+            {/* <Text style={styles.text}>Mario</Text> */}
+            <Image
+              style={styles.logo}
+              source={require("../starwars-app/assets/logostarwars.png")}
+            />
+          </View>
+          <FlatList
+            horizontal
+            data={friends}
+            renderItem={({ item }) => <CardDetail item={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
           />
-        </View>
-        <FlatList
-        style={styles.itemList}
-        data={task}
-        renderItem={Cardetail}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-      />
-      </ImageBackground>
-    </View>
+        </ImageBackground>
+      </View>
+      </SafeAreaView>
+ 
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    borderWidth: 1,
+    borderColor:  "red"
   },
   image: {
     flex: 1,
@@ -54,8 +82,7 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: "100%",
-    height:60,
-   
+    height: 60,
     resizeMode: "contain",
     backgroundColor: "#000000f1",
     flexDirection: "row",
--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -1,39 +1,59 @@
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import {
+  Button,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-const Cardetail = ({url,name}) => {
+const CardDetail = ({ item }) => {
+  const { name, url } = item;
 
-return (
-    <TouchableOpacity>
-        <ImageBackground
-        source={url}
-        style={styles.image}
-        >
-            <View>
-                <Text style={styles.name}>{name}</Text>
-                <Text></Text>
-            </View>
-        </ImageBackground>
+  return (
+    <TouchableOpacity style={styles.touch}>
+      <ImageBackground source={{uri: url}} style={styles.image}>
+        <View style={styles.container}>
+          <Text style={styles.text}>{name}</Text>
+        </View>
+      </ImageBackground>
     </TouchableOpacity>
-)
-  
-}
+   
+  );
+};
 
 const styles = StyleSheet.create({
-    image: {
-        flex: 1,
-        justifyContent: "center",
-        width: "100%",
-        height: "100%",
-        justifyContent: "flex-start",
-      },
-      name: {
-        color: "white",
-        fontSize: 42,
-        lineHeight: 84,
-        fontWeight: "bold",
-        textAlign: "left",
-        backgroundColor: "#000000c1",
-      },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    width: 80,
+    height: 80,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  text: {
+    flex: 1,
+    color: "white",
+    fontSize: 10,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "left",
+    backgroundColor: "#000000c1",
+  },
+  touch: {
+    margin: 3,
+    minWidth: 100,
+    maxWidth: 100,
+    alignItems: "center",
+    height: 90,
+    backgroundColor: "#181818",
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  container: {
+    justifyContent: "flex-start",
+  },
 });
 
-export default Cardetail;
+export default CardDetail;
